e91513e1a6ee727b65f59b3bae5de754d868b445
ysh4296 <dbtmdgns4296@naver.com>
2021-06-24 01:51:41 +0900

    Update donut.cpp

--- donut.cpp
+++ donut.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <windows.h>
-#define Screen_Max_height 21
+#define Screen_Max_height 31
 #define Screen_Max_width 41
 struct Point {
 	float x;
@@ -18,31 +18,30 @@ float z_index[Screen_Max_height][Screen_Max_width] = { 0, };
 Point Rotate_X(Point P,float degree) {
 	Point R = Point(0,0,0);
 	R.x = P.x;
-	R.y = P.y * cos(degree) + P.z * sin(degree);
-	R.z = -P.y * sin(degree) + P.z * cos(degree);
+	R.y = P.y * cosf(degree) + P.z * sinf(degree);
+	R.z = -P.y * sinf(degree) + P.z * cosf(degree);
 	return R;
 }; 
 Point Rotate_Y(Point P, float degree) {
 	Point R = Point(0, 0, 0);
-	R.x = P.x * cos(degree) + P.z * sin(degree);
+	R.x = P.x * cosf(degree) + P.z * sinf(degree);
 	R.y = P.y;
-	R.z = -P.x * sin(degree) + P.z * cos(degree);
+	R.z = -P.x * sinf(degree) + P.z * cosf(degree);
 	return R;
 };
 Point Rotate_Z(Point P, float degree) {
 	Point R = Point(0, 0, 0);
-	R.x = P.x * cos(degree) + P.y * sin(degree);
-	R.y = -P.x * sin(degree) + P.y * cos(degree);
+	R.x = P.x * cosf(degree) + P.y * sinf(degree);
+	R.y = -P.x * sinf(degree) + P.y * cosf(degree);
 	R.z = P.z;
 	return R;
 };
 void print_donut() {
 	for (int r = Screen_Max_height - 1; r >= 0 ; r--) {
-		printf("%d",r);
 		for (int c = 0 ; c < Screen_Max_width ; c++) {
-			printf("%c", Frame_buffer[r][c]);
+			putchar(Frame_buffer[r][c]);
 		}
-		printf("\n");
+		putchar('\n');
 	}
 }
 float Dot_Product(Point A,Point B) {
@@ -52,9 +51,12 @@ float Dot_Product(Point A,Point B) {
 int main() {
 	float xdegree = 0;
 	float zdegree = 0;
+	int cnt = 0;
 	Point init = Point(100, 0, 0);
-	float r = 60;
+	float r = 40;
+	char Bright[13] = ".,-~:;!=*#$@";
 	while (1) {
+		printf("\x1b[2j");
 		for (int r = 0; r < Screen_Max_height; r++) {
 			for (int c = 0; c < Screen_Max_width; c++) {
 				Frame_buffer[r][c] = ' ';
@@ -62,32 +64,30 @@ int main() {
 			}
 		}
 		for (float i = 0; i < 6.28; i += 0.07) {//make a circle
-			Point P = Point(r * cos(i)+init.x, r * sin(i)+init.y, init.z);
+			Point P = Point(r * cosf(i) + init.x, r * sinf(i) + init.y, init.z);
 			for (float j = 0; j < 6.28 ; j += 0.02) {//rotate to y to make a donut
-				P = Rotate_Z(Rotate_X(Rotate_Y(P, j),xdegree),zdegree);
-				float depth = 1 / (P.z+200);
-				int sx = (P.x * 25)/(P.z+200);//screen index x
-				int sy = (P.y * 25)/(P.z+200);//screen index y
-				Point Normal_Vector = Point(cos(i), sin(i), 0);
+				Point Y = Rotate_Z(Rotate_X(Rotate_Y(P, j),xdegree),zdegree);
+				float depth = 1 / (Y.z+200);
+				int sx = (Y.x * 45)/(Y.z+300);//screen index x
+				int sy = (Y.y * 25)/(Y.z+300);//screen index y
+				Point Normal_Vector = Point(cosf(i), sinf(i), 0);
 				Normal_Vector = Rotate_Z(Rotate_X(Rotate_Y(Normal_Vector, j), xdegree), zdegree);
 				float Dot = Dot_Product(Normal_Vector, Point(0, 1, -1));
 				int mx = 20;
-				int my = 10;
+				int my = 15;
 				int Illumination = 8*Dot;
 				if (Illumination < 0) Illumination = 0;
-				if ( mx+sx >= 0 && mx+sx <= 40 && my+sy >= 0 && my+sy <= 20) {
+				if ( mx+sx >= 0 && mx+sx <= 40 && my+sy >= 0 && my+sy <= 30) {
 					if (z_index[my + sy][mx + sx] < depth) {
 						z_index[my + sy][mx + sx] = depth;
-						Frame_buffer[my + sy][mx + sx] = ".,-~:;=!*#$@"[Illumination];
+						Frame_buffer[my + sy][mx + sx] = Bright[Illumination];
 					}
 				}
 			}
 		}
-		Sleep(100);
 		printf("\x1b[H");
 		print_donut();
-		xdegree += 0.04;
-		break;
+		xdegree += 0.15; zdegree -= 0.1;
 	}
 	return 0;
 }
